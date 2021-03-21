1e89cf5e8f6ff6ec1b88da0929ce7756a33ffc52
ENG19CS0272 SAGAR M A <eng19cs0272.masagar@gmail.com>
2021-03-21 21:35:22 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,53 +1,50 @@
 //WAP to find the distance between two points using structures and 4 functions.
- #include<stdio.h>
-#include<math.h>
-
-struct point
+  #include<stdio.h>
+   #include<math.h>
+   struct input_1
 {
-float u1,u2,v1,v2;
-}a,b; 
-
-float input()
+float u1, u2, v1, v2;
+};
+float inputu()
 {
-printf("first point:\n");
-printf("enter value for u:\n");
-scanf("%f",&a.u1);
-printf("enter the value for v:\n");
-scanf("%f",&a.v1);
-
-printf("for second point:\n");
-printf("enter the value for u:\n");
-scanf("%f",&b.u2);
-printf("enter the value for v:\n");
-scanf("%f",&b.v2);
+ 	float u;
+ scanf("%f",&u);
+ 	return u;
+}
+float inputv()
+{
+float v;
+scanf("%f",&v);
+return v;
+}
+float calc(float u1,float v1,float u2,float v2)
+{
+  	 return(sqrt(((u2-u1)*(u2-u1))+((v2-v1)*(v2-v1))));
+}
+ void print(float distance)
+ {
+  	 printf("the distance is =%f",distance);
+}
+ int main()
+{
+ 	 struct input_1 j1,j2;
+ printf("enter coordinate u1");
+ 	 j1.u1=inputu();
+ 	 printf("enter coordinate v1");
+ 	 j1.v1=inputv();
+ 	 printf("enter coordinate u2");
+ 	 j2.u2=inputu();
+ 	 printf("enter coordinate v2");
+ 	 j2.v2=inputv();
+ 	 float distance=calc(j1.u1,j1.v1,j2.u2,j2.v2);
+   	print(distance);
+  	 return 0;
 }
 
-float display(float firstpoint,float secondpoint,float distance)
-{
-
-printf("the distance of two points %f and %f is: %f\n",firstpoint,secondpoint,distance);
-}
-
-float output()
-{
- float firstpoint,secondpoint,distance;
- firstpoint = b.u2 - a.u1;
- secondpoint = b.v2 -a.v1;
- distance = sqrt(firstpoint*firstpoint + secondpoint*secondpoint);
- display(firstpoint,secondpoint,distance);
- return distance;
-}
-
-int main()
-{
-input();
-output();
 
 
-return 0;
-}
 
- 
 
- 
+
+
 
