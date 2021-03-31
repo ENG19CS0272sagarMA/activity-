8cdc138a22d78133c8f1fbcd5ec60e6a99331c0d
ENG19CS0272 SAGAR M A <eng19cs0272.masagar@gmail.com>
2021-03-31 19:54:36 +0530

    Update complex_numbers_sum.c

--- complex_numbers_sum.c
+++ complex_numbers_sum.c
@@ -1,1 +1,38 @@
-//WAP to find the sum of n complex numbers using structures and 4 or more functions.
+ #include<stdio.h>
+
+int display(int addition)
+{
+   printf("the sum of all The given numbers after addition = %d\n",addition);
+}
+ int compute(int array[],int size)
+{
+int addition =0;
+for (int i=0;i<size;i++)
+{
+addition = addition + array[i];
+}
+
+display(addition);
+}
+int input()
+{
+int array[21],size;
+
+printf("enter the size of numbers you want to add:\n");
+scanf("%d",&size);
+
+printf("enter %d elements to add:\n",size);
+for(int i=0;i<size;i++)
+{
+scanf("%d",&array[i]);
+}
+
+compute(array,size);
+}
+int main()
+{
+input();
+}
+
+
+	
